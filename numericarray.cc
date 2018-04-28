4f05bb53b78aaf1dc6128999f8d9d4ec4713b0c9
Philipp Moritz <pcmoritz@gmail.com>
2018-04-27 23:48:04 -0700

    use avx

--- numericarray.cc
+++ numericarray.cc
@@ -1,20 +1,41 @@
 #include <pybind11/pybind11.h>
 #include <pybind11/numpy.h>
 
+#include <x86intrin.h>
+#include <omp.h>
+#define TH_OMP_OVERHEAD_THRESHOLD_VEC_AVX512 4352
+
 #include <iostream>
 
 namespace py = pybind11;
 
+void adds_AVX512(float *y, const float *x, const ptrdiff_t n) {
+  ptrdiff_t i;
+  ptrdiff_t off;
+  int omp_flag = omp_in_parallel();
+  #pragma omp parallel for if ( (n > TH_OMP_OVERHEAD_THRESHOLD_VEC_AVX512) && ( 0 == omp_flag) )private (i)
+  for (i=0; i<=((n)-16); i+=16) {
+    __m512 YMM0;
+    YMM0 = _mm512_loadu_ps(x+i);
+    _mm512_storeu_ps(y+i, YMM0);
+  }
+  off = (n) - ((n)%16);
+  for (i=off; i<(n); i++) {
+    y[i] = x[i];
+  }
+}
+
 void add_arrays(py::array_t<float> input1, py::array_t<float> input2) {
-  auto a = input1.mutable_unchecked<1>();
-  auto b = input1.unchecked<1>();
-  
+  auto buf1 = input1.request(), buf2 = input2.request();
+
   if (input1.ndim() != 1 || input1.ndim() != 1)
     throw std::runtime_error("Number of dimensions must be one");
 
-  for (size_t idx = 0; idx < input1.shape(0); idx++) {
-    a(idx) += b(idx);
-  }
+
+  volatile float *ptr1 = (float *) buf1.ptr;
+  float *ptr2 = (float *) buf2.ptr;
+
+  adds_AVX512(ptr1, ptr2, buf1.shape[0]);
 }
 
 PYBIND11_MODULE(numericarray, m) {
